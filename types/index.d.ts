73d93949585173e7bdf64812d43299317b922726
Josh Ellis <iamjoshellis@gmail.com>
2019-01-14 11:15:12 +1100

    Fixup type defs 🔠

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,5 +8,5 @@ declare module 'react-position-transformer' {
     children: React.ReactNode;
   };
 
-  class Transformer extends React.Component<> {}
+  export default class Transformer extends React.Component<Props, any> {}
 }
