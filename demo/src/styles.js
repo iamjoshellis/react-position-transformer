2b26de6cb10783d5790e6af7c389f4110ff6f018
Josh Ellis <iamjoshellis@gmail.com>
2019-03-13 17:00:31 +1100

    Update demo styles 🎨

--- demo/src/styles.js
+++ demo/src/styles.js
@@ -1,9 +1,16 @@
 import styled, { css, createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
   html,
   body,
   #root {
+    margin: 0;
+    padding: 0;
     height: 100%;
     width: 100%;
     overflow-x: hidden;
